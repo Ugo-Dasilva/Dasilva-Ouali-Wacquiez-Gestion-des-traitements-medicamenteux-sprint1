adaefd823415e79ee9fbdf7a41fca090265ce392
Antoine-Wacquiez <wacquiez.antoine@gmail.com>
2022-11-30 15:47:19 +0100

    nav + page d'accueil + les traitements

--- src/Controller/TraitementController.php
+++ src/Controller/TraitementController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Traitement;
 
 
 class TraitementController extends AbstractController
