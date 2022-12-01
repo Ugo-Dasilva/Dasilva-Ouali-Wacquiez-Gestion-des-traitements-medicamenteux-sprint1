e5f5d1407c1490501b2e7adf1baa88e29a7e5f0a
Ugo-Dasilva <ugodasilva1@gmail.com>
2022-12-01 16:07:34 +0100

    ajout formulaire CUD medicament et sejour

--- src/Controller/MedicamentController.php
+++ src/Controller/MedicamentController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Medicament;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\MedicamentType;
 
 class MedicamentController extends AbstractController
 {
@@ -43,6 +45,56 @@ public function AfficherLeMedicament(ManagerRegistry $doctrine,$id): Response
     'medicament' => $unMedicament,
     ]);
 }
+#[Route('/formulaire_add_medicament' , name: 'formulaire_add_medicament')]
+    public function formulaire_ajoutMed(ManagerRegistry $doctrine , Request $request):Response {
+        $em=$doctrine->getManager();
+        $Medicament=new Medicament();
+        $form =$this->createForm(MedicamentType::class,$Medicament);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $Medicament=$form->getData();
+            $em->persist($Medicament);
+            $em->flush();
+            return $this->redirectToRoute('app_medicament');
+        }
+        return $this->render ('medicament/formulaire.html.twig',array(
+            'form'=> $form->createView(),
+        ));
+
+        
+
+        }
+        #[Route('/formulaire_modif_medicament/{id}' , name: 'formulaire_modif_medicament')]
+    public function formulaire_modifMed(ManagerRegistry $doctrine , $id,Request $request):Response {
+        $em=$doctrine->getManager();
+        $repository=$doctrine->getRepository(Medicament::class);
+        $Medicament=$repository->find($id);
+        $form =$this->createForm(MedicamentType::class,$Medicament);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $Medicament=$form->getData();
+            $em->persist($Medicament);
+            $em->flush();
+            return $this->redirectToRoute('app_medicament');
+        }
+        return $this->render ('medicament/formulaire.html.twig',array(
+            'form'=> $form->createView(),
+        ));
+
+        
+
+        }
+        #[Route('/suppMedicament/{id}', name: 'app_suppMedicament')]
+        public function SuppMedicament(ManagerRegistry $doctrine,$id): Response
+        {
+           $repository=$doctrine->getRepository(Medicament::class);
+           // Récupération de tous les adhérents
+           $Medicament=$repository->find($id);
+           $em=$doctrine->getManager();
+           $em->remove($Medicament);
+           $em->flush();
+           return $this->redirectToRoute('app_medicament');
+        }
 
 
 }
--- src/Controller/SejourController.php
+++ src/Controller/SejourController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Sejour;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\SejourType;
 
 class SejourController extends AbstractController
 {
@@ -41,4 +43,54 @@ class SejourController extends AbstractController
     'sejour' => $unSejour,
     ]);
 }
+#[Route('/formulaire_add_sejour' , name: 'formulaire_add_sejour')]
+    public function formulaire_ajoutSej(ManagerRegistry $doctrine , Request $request):Response {
+        $em=$doctrine->getManager();
+        $Sejour=new Sejour();
+        $form =$this->createForm(SejourType::class,$Sejour);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $Sejour=$form->getData();
+            $em->persist($Sejour);
+            $em->flush();
+            return $this->redirectToRoute('app_sejours');
+        }
+        return $this->render ('sejour/formulaire.html.twig',array(
+            'form'=> $form->createView(),
+        ));
+
+        
+
+        }
+        #[Route('/formulaire_modif_sejour/{id}' , name: 'formulaire_modif_sejour')]
+    public function formulaire_modifMed(ManagerRegistry $doctrine , $id,Request $request):Response {
+        $em=$doctrine->getManager();
+        $repository=$doctrine->getRepository(Sejour::class);
+        $Sejour=$repository->find($id);
+        $form =$this->createForm(SejourType::class,$Sejour);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $Sejour=$form->getData();
+            $em->persist($Sejour);
+            $em->flush();
+            return $this->redirectToRoute('app_sejours');
+        }
+        return $this->render ('sejour/formulaire.html.twig',array(
+            'form'=> $form->createView(),
+        ));
+
+        
+
+        }
+        #[Route('/suppSejour/{id}', name: 'app_suppSejour')]
+        public function SuppSejour(ManagerRegistry $doctrine,$id): Response
+        {
+           $repository=$doctrine->getRepository(Sejour::class);
+           // Récupération de tous les adhérents
+           $Sejour=$repository->find($id);
+           $em=$doctrine->getManager();
+           $em->remove($Sejour);
+           $em->flush();
+           return $this->redirectToRoute('app_sejours');
+        }
 }
