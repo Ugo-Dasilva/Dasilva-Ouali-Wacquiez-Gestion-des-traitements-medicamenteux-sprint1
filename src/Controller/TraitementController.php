b8fd185b7b3cf09a8aa122263c9e516b571cfeab
Antoine-Wacquiez <wacquiez.antoine@gmail.com>
2022-12-01 16:03:04 +0100

    ajout

--- src/Controller/TraitementController.php
+++ src/Controller/TraitementController.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Traitement;
+use App\Entity\Sejour;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\HttpFoundation\Request;
 
 class TraitementController extends AbstractController
@@ -53,7 +55,7 @@ class TraitementController extends AbstractController
         $form = $this->createFormBuilder($traitement)
         ->add('datedebut',DateType::class, array('label'=>'Date de début du traitement : '))
         ->add('datefin',DateType::class, array('label'=>'Date de fin du traitement : '))
-        ->add('sejour',DateType::class, array('label'=>'L\'id du séjour : '))
+        ->add('sejour',EntityType::class,array('class'=>Sejour::class,'choice_label'=>'id'))
         ->add('save', SubmitType::class, array('label' => 'Enregistrer le traitement'))
         ->getForm();
         $form->handleRequest($request);
@@ -71,4 +73,42 @@ class TraitementController extends AbstractController
     }
 
     
+    #[Route('/modifTraitement/{id}', name: 'app_modifTraitement')]
+    public function ModifierUnTraitement(ManagerRegistry $doctrine,Request $request, $id): Response
+    {
+    $repository=$doctrine->getRepository(Traitement::class);
+    $em=$doctrine->getManager();
+    $traitement=$repository->find($id);
+    $form = $this->createFormBuilder($traitement)
+    ->add('datedebut',DateType::class, array('label'=>'Date de début du traitement : '))
+    ->add('datefin',DateType::class, array('label'=>'Date de fin du traitement : '))
+    ->add('sejour',EntityType::class,array('class'=>Sejour::class,'choice_label'=>'id'))
+    ->add('save', SubmitType::class, array('label' => 'Modifier le traitement'))
+    ->getForm();
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $traitement = $form->getData();
+        $em->persist($traitement);
+        $em->flush();
+        // redirection vers la liste des adhérents
+        return $this->redirectToRoute('app_traitements');
+    }
+    else{
+        return $this->render('traitement/modifTraitement.html.twig', array(
+    'form' => $form->createView(),));
+    }
+    }
+
+    #[Route('/suppTraitement/{id}', name: 'app_suppTraitement')]
+        public function indexSuppAdherent(ManagerRegistry $doctrine,$id): Response
+        {
+           $repository=$doctrine->getRepository(Traitement::class);
+           // Récupération de tous les adhérents
+           $unTraitement=$repository->find($id);
+           $em=$doctrine->getManager();
+           $em->remove($unTraitement);
+           $em->flush();
+           return $this->redirectToRoute('app_traitements');
+        }
+
 }
