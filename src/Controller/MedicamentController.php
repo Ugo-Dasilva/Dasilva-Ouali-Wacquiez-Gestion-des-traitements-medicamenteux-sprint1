b5f6e303dcf818649408551514dc3b78e2a56cc0
Ugo-Dasilva <ugodasilva1@gmail.com>
2022-12-01 13:45:54 +0100

    ajout medicament

--- src/Controller/MedicamentController.php
+++ src/Controller/MedicamentController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Medicament;
 
 class MedicamentController extends AbstractController
 {
@@ -24,12 +25,24 @@ class MedicamentController extends AbstractController
         $lesMedicaments = $repository -> findAll();
         return $this->render('medicament/affichage.html.twig', [
             'controller_name' => 'MedicamentController',
-            'Medicaments' => $lesMedicaments,
+            'lesMedicaments' => $lesMedicaments,
         ]);
 
 
 
     }
+
+     
+#[Route('/affichageUnMedicament/{id}', name: 'app_medicamentId')]
+public function AfficherLeTraitement(ManagerRegistry $doctrine,$id): Response
+{
+   $repository=$doctrine->getRepository(Medicament::class);
+   
+   $unMedicament=$repository->find($id);
+   return $this->render('medicament/medicamentAvecId.html.twig', [
+    'medicament' => $unMedicament,
+    ]);
+}
 
 
 }
